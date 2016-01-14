0ff058abb20fcd7352ec346885ec5bcdaaf993f6
BearJ <bearverygood@126.com>
2016-01-14 17:48:53 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
 gulp.task('default', ['server'], function () {
-    gulp.watch('/**/*.*', function () {
+    gulp.watch(['index.html', 'css/**/*.*', 'html/**/*.*', 'img/**/*.*', 'js/**/*.*'], function () {
         browserSync.reload();
     });
 });
